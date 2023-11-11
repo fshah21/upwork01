4dcb53cf20af0f45a98d4dcfb6d7bf67cec73d09
Feya Shah <feya.shah@askpinkypromise.com>
2023-11-11 10:55:10 +0530

    Adding a basic site

---
+++ src/data.js
@@ -0,0 +1,61 @@
+export const projects = [
+  {
+    title: "NewsApp",
+    subtitle: "React, Bootstrap and Nodejs",
+    description:
+      "The aim of the project was to develop a mobile friendly website powered by React and NodeJS which helped users to receive news updates with enhanced functionalities like commenting and bookmarking their favorite articles.",
+    image: "./newsapp.png",
+    link: " https://devpost.com/software/newsapp-yau6lbm",
+  },
+  {
+    title: "BeSexEd",
+    subtitle: "HTML, CSS, Javascript and AJAX",
+    description:
+      "The project focused on making sex education interactive, fun and accessible. Our web application was customized using HTML5, CSS3, Javascript and AJAX to incorporate multiple features which would enable users to participate in quizzes and engage in relevant conversations.",
+    image: "./besexed.png",
+    link: "https://devpost.com/software/be-sexed-kgq5b2",
+  },
+  {
+    title: "My Real Life Version",
+    subtitle: "Wordpress",
+    description:
+      "A personal blog website writing about my personal experiences and opinions.",
+    image: "./reallifeversion.png",
+    link: "https://myreallifeversion.com",
+  },
+  {
+    title: "Harexi",
+    subtitle: "React Native, Nodejs and Firebase",
+    description:
+      "The aim of the project was to develop a platform agnostic app powered by React Native and Firebase which helps users to make healthier food choices. Lead the team of seven people and interacted with a real client.",
+    image: "./harexi.png",
+    link: "https://devpost.com/software/harexi",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Go",
+  "Node",
+  "Java",
+  "AWS",
+  "MySQL",
+];
