d5ebf6e8f21f3b23f238d0490c8bfcf478fdcd10
Feya Shah <feya.shah@askpinkypromise.com>
2023-12-10 14:07:24 +0530

    Styling images

---
+++ build/precache-manifest.cadf0b76e42d1dc3208ba64714d4b3ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccaad1e2ce82f94a8a68",
+    "url": "/upwork01/static/css/main.3e8a8d35.chunk.css"
+  },
+  {
+    "revision": "ccaad1e2ce82f94a8a68",
+    "url": "/upwork01/static/js/main.6c21793b.chunk.js"
+  },
+  {
+    "revision": "cebf9fff4c8641182a60",
+    "url": "/upwork01/static/js/runtime~main.2d5cf5e3.js"
+  },
+  {
+    "revision": "e82eb6847ca4fe8060aa",
+    "url": "/upwork01/static/js/2.2c18a87d.chunk.js"
+  },
+  {
+    "revision": "7af31bf5e7bd64e422b21d23abfa031b",
+    "url": "/upwork01/static/media/cv.7af31bf5.pdf"
+  },
+  {
+    "revision": "9a47288209f54282937475681ca80645",
+    "url": "/upwork01/index.html"
+  }
+];
